ba3fe7350378b975eb068f8f84d36ff73c00c6bb
tobiaserich <56834185+tobiaserich@users.noreply.github.com>
2019-12-20 16:47:54 +0100

    Add a11y addon to storybook and readjust the other storybook files to this (#33)

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -4,6 +4,7 @@ import GlobalStyles from "../src/GlobalStyles";
 import { withKnobs } from "@storybook/addon-knobs";
 import normal from "../src/themes/normal";
 import { ThemeProvider } from "emotion-theming";
+import { withA11y } from "@storybook/addon-a11y";
 // add GlobalStyle for every story
 const GlobalStyleDecorator = storyFn => (
   <ThemeProvider theme={normal}>
@@ -13,6 +14,7 @@ const GlobalStyleDecorator = storyFn => (
 );
 addDecorator(GlobalStyleDecorator);
 addDecorator(withKnobs);
+addDecorator(withA11y);
 
 // automatically import all files ending in *.stories.js
 configure(require.context("../src/stories", true, /\.stories\.js$/), module);
