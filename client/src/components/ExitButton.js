89195db993aef146fc3d7c893eb9320569f96bf9
tobiaserich <tobiaserich@hotmail.de>
2019-12-22 21:08:11 +0100

    Change the last file to destructure...

--- client/src/components/ExitButton.js
+++ client/src/components/ExitButton.js
@@ -8,18 +8,18 @@ const ExitButtonContainer = styled.div`
   position: absolute;
   top: 5px;
   right: 5px;
-  border: 1px solid ${props => props.theme.colors.darkfont};
-  background: ${props => props.theme.colors.font};
+  border: 1px solid ${({ theme }) => theme.colors.darkfont};
+  background: ${({ theme }) => theme.colors.font};
 `;
 
 const ExitButtonLine = styled.div`
   width: 80%;
   height: 2px;
-  background: ${props => props.theme.colors.darkfont};
+  background: ${({ theme }) => theme.colors.darkfont};
   z-index: 9995;
   border-radius: 10px;
-  ${props => {
-    if (props.direction === "left") {
+  ${({ direction }) => {
+    if (direction === "left") {
       return "transform: translateY(8px) translateX(2px) rotate(45deg)";
     } else {
       return "transform:translateY(6px) translateX(2px) rotate(-45deg) ";
