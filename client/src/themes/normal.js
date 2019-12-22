eab9f16ae7397c3bbdb711873d708f374694a92c
tobiaserich <tobiaserich@hotmail.de>
2019-12-22 17:20:49 +0100

    adjust the background color to match WCAG 2 guideline

--- client/src/themes/normal.js
+++ client/src/themes/normal.js
@@ -1,7 +1,8 @@
 const normal = {
   colors: {
     active: "#F7F9FC",
-    background: "#BAE0FF",
+    background: "#254B74",
+    backgroundTransparent: "#254B74CC",
     font: "#F7F9FC",
     darkfont: "#707070"
   }
