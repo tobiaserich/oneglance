4260917d3a3c3452b14959a9c02fa461137a5c03
tobiaserich <tobiaserich@hotmail.de>
2019-12-23 12:26:11 +0100

    Add profile menu to storybook

---
+++ client/src/stories/mainSite/6-profileInformation.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import ProfileNavigation from "../../components/ProfileNavigation";
+
+export default {
+  title: "Main Page|Profile Navigation"
+};
+
+export function profileNavigation() {
+  return <ProfileNavigation />;
+}
