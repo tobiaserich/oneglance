01a81272c8a12f39aea3c016300f16a77ce0fb4d
tobiaserich <tobiaserich@hotmail.de>
2019-12-26 18:15:42 +0100

    kill the pending status

--- client/src/components/UserDetailImg.js
+++ client/src/components/UserDetailImg.js
@@ -3,7 +3,6 @@ import styled from "@emotion/styled";
 import UserImg from "./UserImg";
 import blank from "../assets/icons/errorIcon.svg";
 import finished from "../assets/icons/doneIcon.svg";
-import pending from "../assets/icons/pendingIcon.svg";
 
 const StatusImg = styled.img`
   width: 15px;
@@ -11,14 +10,13 @@ const StatusImg = styled.img`
   position: absolute;
   right: 0;
   bottom: 0;
-  background-color: ${({ status }) => (status === "pending" ? "#e5ca00" : "#fff")};
+  background-color: #fff;
   border-radius: 50%;
 `;
 
 const pollStatus = {
   blank: blank,
-  finished: finished,
-  pending: pending
+  finished: finished
 };
 export default function UserDetailImg({ status, username }) {
   const statusReport = `PollStatus for User ${username}: ${status}`;
