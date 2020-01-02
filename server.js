205b4ecc5f9fa7d1264e8283643bc6055af8db50
tobiaserich <tobiaserich@hotmail.de>
2020-01-02 17:16:52 +0100

    add new routes

--- server.js
+++ server.js
@@ -2,11 +2,21 @@ require("dotenv").config();
 const express = require("express");
 const { dbInit } = require("./lib/db");
 const app = express();
-const { setEvent } = require("./lib/events");
+const { setEvent, getOwnEvents } = require("./lib/events");
 
 app.use(express.json({ extended: false }));
 
 app.get("/api/addEvent", (req, res) => res.send("geht"));
+
+app.get("/api/events/:username", async (req, res) => {
+  try {
+    const result = await getOwnEvents(req.params.username);
+    res.send(result);
+  } catch (error) {
+    console.error(error);
+    res.send(error);
+  }
+});
 
 app.post("/api/addEvent", (req, res) => {
   const eventDatas = req.body;
