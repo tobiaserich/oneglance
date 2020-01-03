cf0b1584b50e2612c0a63aa2925ece22ed8b85db
tobiaserich <56834185+tobiaserich@users.noreply.github.com>
2020-01-03 13:23:25 +0100

    Server routes (#58)
    * Improve server routes and combine the api event functions
    
    * Change the import

--- client/src/api/event.js
+++ client/src/api/event.js
@@ -1,10 +1,16 @@
 export function setEvent(title, date, descr, background, user) {
   const datas = { title, date, descr, background, owner: user };
-  return fetch("/api/addEvent", {
+  return fetch("/api/event", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
     body: JSON.stringify(datas)
   });
+}
+
+export async function getOwnEvents(userName) {
+  const response = await fetch(`/api/event/:${userName}`);
+  const datas = await response.json();
+  return datas;
 }
