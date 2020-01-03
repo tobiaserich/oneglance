25e5bd6a5dfd65e564ee723b0fd9d6efc1e0729b
tobiaserich <tobiaserich@hotmail.de>
2020-01-03 11:50:27 +0100

    Change proptypes, enlarge the menu button field, extend the item component

--- client/src/components/MenuItem.js
+++ client/src/components/MenuItem.js
@@ -3,18 +3,29 @@ import styled from "@emotion/styled";
 import PropTypes from "prop-types";
 
 const Item = styled.div`
-
   margin-top: ${({ spacer }) => {
     return spacer + "px";
   }};
   font-size: 16px;
   margin-left: 5px;
   color: ${({ theme }) => theme.colors.font};
-
 `;
 
-export default function menuItem({ children, spacer }) {
-  return <Item spacer={spacer}>{children}</Item>;
+export default function menuItem({ children, spacer, onClick, target }) {
+  if (onClick) {
+    return (
+      <Item
+        spacer={spacer}
+        onClick={() => {
+          onClick(target);
+        }}
+      >
+        {children}
+      </Item>
+    );
+  } else {
+    return <Item spacer={spacer}>{children}</Item>;
+  }
 }
 
 menuItem.propTypes = {
