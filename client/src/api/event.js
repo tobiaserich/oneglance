502a398754aa71009aa22962167e6f631903399b
tobiaserich <tobiaserich@hotmail.de>
2020-01-03 15:54:35 +0100

    Add the delete route and delete function

--- client/src/api/event.js
+++ client/src/api/event.js
@@ -1,16 +1,23 @@
 export function setEvent(title, date, descr, background, user) {
-  const datas = { title, date, descr, background, owner: user };
+  const data = { title, date, descr, background, owner: user };
   return fetch("/api/event", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify(datas)
+    body: JSON.stringify(data)
+  });
+}
+
+export function deleteEvent(eventID) {
+  console.log();
+  return fetch(`/api/event/del/${eventID}`, {
+    method: "DELETE"
   });
 }
 
 export async function getOwnEvents(userName) {
-  const response = await fetch(`/api/event/:${userName}`);
-  const datas = await response.json();
-  return datas;
+  const response = await fetch(`/api/event/${userName}`);
+  const data = await response.json();
+  return data;
 }
