33494bc97f7c5ecb422f7c302023cb53e5d3df86
tobiaserich <56834185+tobiaserich@users.noreply.github.com>
2020-01-04 14:21:55 +0100

    Delete event (#62)
    * Add the delete route and delete function
    
    * Better naming
    
    * Delete console log
    
    * Add the delete function and automatic rerendering after delete.
    Also add the redirect function to the add Event site
    
    * Add rerendering of the events after delete an event

--- client/src/components/Cards.js
+++ client/src/components/Cards.js
@@ -1,7 +1,8 @@
 import React from "react";
 import styled from "@emotion/styled";
 import Card from "../components/Card";
-import getOwnEvents from "../api/getOwnEvents";
+import { getOwnEvents } from "../api/event";
+import UserContext from "../hooks/UserContext";
 
 const CardsContainer = styled.div`
   margin-top: 10px;
@@ -12,20 +13,29 @@ const CardsContainer = styled.div`
 
 export default function Cards() {
   const [events, setEvents] = React.useState([]);
+  const userName = React.useContext(UserContext);
 
   React.useEffect(() => {
     async function fetcher() {
-      const results = await getOwnEvents();
+      const results = await getOwnEvents(userName);
       setEvents(results);
     }
     fetcher();
-  }, []);
+  }, [events]);
 
   return (
     <CardsContainer>
       {events.map(event => {
-        const eventDatas = { title: event.title, date: event.date, descrition: event.description };
-        return <Card key={event._id} imgsrc={event.background} eventDatas={eventDatas}></Card>;
+        const eventDatas = { title: event.title, date: event.date, description: event.descr };
+        return (
+          <Card
+            key={event._id}
+            eventID={event._id}
+            imgsrc={event.background}
+            eventDatas={eventDatas}
+            events={setEvents}
+          ></Card>
+        );
       })}
     </CardsContainer>
   );
