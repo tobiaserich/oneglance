54669dfcdb8b2da1e982b83f8c3798854d76a29f
tobiaserich <tobiaserich@hotmail.de>
2020-01-06 13:27:58 +0100

    Combine some container

--- client/src/components/DetailContainer.js
+++ client/src/components/DetailContainer.js
@@ -6,6 +6,16 @@ export const DetailContainer = styled.article`
   margin-top: 10px;
 `;
 
+export const DetailEventContainer = styled.article`
+  height: 90%;
+  width: 90%;
+
+  display: flex;
+  margin: auto;
+  border-radius: 10px;
+  padding: 5px 10px;
+`;
+
 DetailContainer.propTypes = {
   children: PropTypes.node
 };
