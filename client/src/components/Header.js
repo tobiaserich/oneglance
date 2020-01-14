5a7f29917288df01c6378f157e0ba359cb99c886
tobiaserich <56834185+tobiaserich@users.noreply.github.com>
2020-01-14 19:16:23 +0100

    Readjust the headers design a litle bit (#79)

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,22 +6,25 @@ import UserContext from "../hooks/UserContext";
 const HeaderContainer = styled.header`
   height: 10%;
   width: 100%;
-  background-color: ${({ theme }) => theme.colors.backgroundTransparent};
+  position: relative;
   display: flex;
   align-items: center;
   justify-content: space-between;
+  background-color: ${({ theme }) => theme.colors.backgroundTransparent};
   color: ${({ theme }) => theme.colors.font};
   font-family: "Great Vibes";
   font-size: 35px;
   letter-spacing: 1px;
 `;
-
+const LogoWrapper = styled.div`
+  margin: auto;
+`;
 export default function Header() {
   const userName = React.useContext(UserContext);
   return (
     <HeaderContainer>
-      <UserImg thread="header" username={userName} />
-      <div>oneGlance</div>
+      <UserImg thread="header" username={userName} position="absolute" />
+      <LogoWrapper>oneGlance</LogoWrapper>
       <div></div>
     </HeaderContainer>
   );
