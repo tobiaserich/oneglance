33fe166b0a63f96b86cbea5b4b0350875188b46a
tobiaserich <tobiaserich@hotmail.de>
2020-01-15 17:03:39 +0100

    Change the routing to the pages and use useParams

--- client/src/pages/Poll.js
+++ client/src/pages/Poll.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { useHistory } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import { addPoll } from "../api/poll";
 import { getPoll } from "../api/poll";
 import { AddContainer, FlexContainer } from "../components/Container";
@@ -15,8 +15,8 @@ const Spacer = styled.div`
 `;
 
 export default function Poll() {
-  const eventID = new URLSearchParams(window.location.search).get("ID");
-  const pollID = new URLSearchParams(window.location.search).get("poll");
+  const { eventID } = useParams();
+  const { pollID } = useParams();
   const [title, setTitle] = React.useState("");
   const [totalPoll, setTotalPoll] = React.useState([{ question: "", answers: [] }]);
   const [currentQuestion, setCurrentQuestion] = React.useState(0);
@@ -46,7 +46,6 @@ export default function Poll() {
   function handleQuestionClick(direction) {
     if (direction === "prev") {
       setCurrentQuestion(currentQuestion - 1);
-      console.log("fire");
     } else if (direction === "next") {
       const poll = [...totalPoll];
       const newQuestion = { question: "", answers: [] };
@@ -65,7 +64,7 @@ export default function Poll() {
 
   function handleSubmit() {
     addPoll(totalPoll, eventID, title, pollID);
-    history.push(`../eventDetails/?ID=${eventID}`);
+    history.push(`/eventDetails/${eventID}`);
   }
   return (
     <AddContainer>
@@ -134,7 +133,7 @@ export default function Poll() {
         New answer
       </NewEntryButton>
       <FlexContainer>
-        <FlexButton type="button" onClick={() => history.push(`../eventDetails/?ID=${eventID}`)}>
+        <FlexButton type="button" onClick={() => history.push(`/eventDetails/${eventID}`)}>
           Discard
         </FlexButton>
         <FlexButton
