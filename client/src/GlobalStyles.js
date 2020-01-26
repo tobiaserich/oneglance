7932a94936860d4867c2864b0670cbce1283c73b
TobiasErich <TobiasErich@hotmail.de>
2020-01-26 21:55:31 +0100

    Hide scrollbar in Firefox

--- client/src/GlobalStyles.js
+++ client/src/GlobalStyles.js
@@ -17,6 +17,7 @@ function GlobalStyles() {
           width: 100vw;
           font-family: "roboto", "sans-serif";
           background-color: ${theme.colors.background};
+          scrollbar-width: none;
         }
         #root {
           margin: auto;
